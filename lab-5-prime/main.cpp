2f237b063cd60bb5f5a414c9f34f9b22adb419bf
OmarAdel90 <omaradel964@gmail.com>
2023-04-11 22:47:59 +0200

    Finished prime finder

--- lab-5-prime/main.cpp
+++ lab-5-prime/main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int firstNum , secondNum;
-    int total = 0;
-    int flag = 0;
-    
+int main() {
+       int run = 0;
+    do
+    {
+        int firstNum, secondNum;
+        int total = 0;
+        bool flag = true;
         cout << "Enter 1st number" << endl;
         cin >> firstNum;
         cout << "Enter 2nd number" << endl;
         cin >> secondNum;
-        for(int i = firstNum; i <= secondNum; i++){
-            for (int j = 2; j * j <= i; j++)
+
+    for (int i = firstNum; i <= secondNum; i++) {
+        flag = true;
+        if (i == 1 || i == 2 || i <= 0)
+            flag = false;
+        else
+        {
+            for (int j = 2; j < i; j++)
             {
-                if(i % j == 0){
+                if (i % j == 0) {
+                    flag = false;
                     break;
                 }
-                else{
-                    total += 1;
-                    break;
-                }
+
             }
         }
-    
+        if (flag)
+            total += 1;
+    }
     cout << "number of primes is " << total << endl;
-    
+    cout << "to stop running press 1 to continue press 0" << endl;
+    cin >> run;
+    } while (run == 0);
 }
